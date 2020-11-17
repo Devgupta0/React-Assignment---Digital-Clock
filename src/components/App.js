50d3d5371e3e1b509400d250fc540e7299d026d0
Devgupta0 <52696089+Devgupta0@users.noreply.github.com>
2020-11-18 00:04:49 +0530

    clock

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,44 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            time : new Date(),
-        };
-        this.intervalId = null;
-    } 
-    
-    
-    getTimeString(){
-        const currTime = this.state.time;
-        const pad = (x) => x.toString().padStart(2,"0");
-        const [hours,minutes,seconds] = [currTime.getHours(),currTime.getMinutes(),currTime.getSeconds()];
-        const amORPm = hours >= 12?"PM":"AM";
-        const twelveHourFormat = hours>12 ? hours-12:hours;
-        
-        let timeString = `${pad(twelveHourFormat)}:${pad(minutes)}:${pad(seconds)}${amORPm}`;
-        return timeString;
-    }
-    componentDidMount(){
-            this.intervalId = setInterval(() =>{
-                this.setState({
-                    time: new Date()
-                })
-            },1*1000);
-
-        }
-    componentWillUnmount(){
-        clearInterval(this.intervalId);
-    }
-    render() {
-
-        return(
-            <div className="Clock">
-               <h3 id="time">{this.getTimeString()}</h3>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date(),
+    };
+    this.intervalId = null;
+  }
+  getTimeString() {
+    const currentTime = this.state.time;
+    const pad = (x) => x.toString().padStart(2, "0");
+    const [hrs, min, sec] = [
+      currentTime.getHours(),
+      currentTime.getMinutes(),
+      currentTime.getSeconds(),
+    ];
+    const pm = hrs >= 12 ? " PM" : " AM";
+    const hr = hrs > 12 ? hrs - 12 : hrs;
+    let string = `${pad(hr)}:${pad(min)}:${pad(sec)}${pm}`;
+    return string;
+  }
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({
+        time: new Date(),
+      });
+    }, 1 * 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.getTimeString()}</h3>
+      </div>
+    );
+  }
 }
-
 
 export default App;
